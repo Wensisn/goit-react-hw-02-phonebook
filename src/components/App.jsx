b3b1439a984d499719d90974ff25f6ce7202fcb0
Zubenko1 <zubenkodp@gmail.com>
2022-10-16 16:11:58 +0300

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,49 @@
 import { Component } from 'react';
 import { FormPhone } from './Form/Form';
+import { TodoList } from './TodoList/TodoList';
+import { nanoid } from 'nanoid';
+import arrayContact from './todo.json';
 
 export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  addTodo = ({ name, number }) => {
+    if (this.isExistContact(name)) {
+      alert`${name} такой уже есть`;
+      return false;
+    }
+    const todo = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    // НЕ РЕБОТАЕТ !
+
+    console.log(
+      this.setState(prevState => ({
+        contacts: [todo, ...prevState.contacts],
+      }))
+    );
+  };
+
+  deleteContact = TodoId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(todo => todo.id !== TodoId),
+    }));
+  };
+
+  isExistContact = name => {
+    return this.state.contacts.some(item => item.name === name);
+  };
+
   onSubmitHandel = data => {
     setTimeout(() => {
       console.log(data);
@@ -9,9 +51,11 @@ export class App extends Component {
   };
 
   render() {
+    const { contacts } = this.state;
     return (
       <>
-        <FormPhone onSubmit={this.onSubmitHandel} />
+        <FormPhone contacts={contacts} onSubmit={this.addTodo} />
+        <TodoList contacts={contacts} onDeleteContact={this.deleteContact} />
       </>
     );
   }
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,34 +1,15 @@
 import { Component } from 'react';
-import { TodoList } from '../TodoList/TodoList';
 import { nanoid } from 'nanoid';
-import arrayContact from '../todo.json';
+import PropTypes from 'prop-types';
+import React from 'react';
 import css from './Form.module.css';
 
 export class FormPhone extends Component {
-  state = {
-    contacts: arrayContact,
-    filter: '',
-    name: '',
-    number: '',
+  static propTypes = {
+    onSubmit: PropTypes.func.isRequired,
   };
 
-  addTodo = text => {
-    const todo = {
-      id: nanoid(),
-      name: this.state.name,
-      number: this.state.number,
-    };
-
-    this.setState(prevState => ({
-      contacts: [todo, ...prevState.contacts],
-    }));
-  };
-
-  deleteContact = TodoId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(todo => todo.id !== TodoId),
-    }));
-  };
+  state = { name: '', number: '' };
 
   handelChange = event => {
     const { name, value } = event.currentTarget;
@@ -39,10 +20,14 @@ export class FormPhone extends Component {
 
   onSubmitnForm = event => {
     event.preventDefault();
-    this.addTodo();
-    this.props.onSubmit(this.state.contacts);
-    this.reset();
+    if (this.props.onSubmit(this.state)) this.reset();
+
+    console.log(this.state);
+    // НЕ РЕБОТАЕТ СОЗДАНИЕ НОВОГО КОНТАКТА!
   };
+
+  // const { name, number } = this.state;
+  // console.log(this.props.onSubmit(name, number));
 
   reset = () => {
     this.setState({
@@ -52,16 +37,18 @@ export class FormPhone extends Component {
   };
 
   render() {
-    const { contacts } = this.state;
+    const nameInputId = nanoid();
+    const numberInputId = nanoid();
+
     return (
       <div className={css.sectionForm}>
         <h2>PhoneBook</h2>
 
         <form className={css.form} onSubmit={this.onSubmitnForm}>
-          <label htmlFor={nanoid()}>
+          <label htmlFor={nameInputId}>
             <span className={css.name}>Name</span>
             <input
-              id={nanoid()}
+              id={nameInputId}
               value={this.state.name}
               type="text"
               name="name"
@@ -69,10 +56,10 @@ export class FormPhone extends Component {
               onChange={this.handelChange}
             />
           </label>
-          <label htmlFor={nanoid()}>
+          <label htmlFor={numberInputId}>
             <span className={css.name}>Number</span>
             <input
-              id={nanoid()}
+              id={numberInputId}
               value={this.state.number}
               type="tel"
               name="number"
@@ -82,13 +69,15 @@ export class FormPhone extends Component {
               onChange={this.handelChange}
             />
           </label>
-          <button className={css.click}>Нажми на меня</button>
+          <button type="submit" className={css.click}>
+            Нажми на меня
+          </button>
         </form>
         {/* <Search /> */}
         <div>
-          <span>Общее: {contacts.length}</span>
+          {/* <span>Общее: {contacts.length}</span> */}
           {/* <span>Общее не выполн: {a.length}</span> */}
-          <TodoList contacts={contacts} onDeleteContact={this.deleteContact} />
+          {/* <TodoList contacts={contacts} onDeleteContact={this.deleteContact} /> */}
         </div>
       </div>
     );
